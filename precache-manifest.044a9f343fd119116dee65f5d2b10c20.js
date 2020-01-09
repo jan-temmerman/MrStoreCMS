963fb22f56862f784a124b68c3f9434dcf6a5e2c
Jan Temmerman <jantemme@student.arteveldehs.be>
2020-01-09 16:46:16 +0100

    Updates

---
+++ precache-manifest.044a9f343fd119116dee65f5d2b10c20.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d4f705bec3ea8864c318c03a3355874",
+    "url": "/.github.io/MrStoreCMS/index.html"
+  },
+  {
+    "revision": "8d2d968b0f24cd0ef8e0",
+    "url": "/.github.io/MrStoreCMS/static/css/main.0ba02492.chunk.css"
+  },
+  {
+    "revision": "f0f101979179a28ff276",
+    "url": "/.github.io/MrStoreCMS/static/js/2.018ea000.chunk.js"
+  },
+  {
+    "revision": "b03343d1dfdf0242e0f4ba3806a19844",
+    "url": "/.github.io/MrStoreCMS/static/js/2.018ea000.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8d2d968b0f24cd0ef8e0",
+    "url": "/.github.io/MrStoreCMS/static/js/main.0b22d57e.chunk.js"
+  },
+  {
+    "revision": "f3030fdd5acfe3c68597",
+    "url": "/.github.io/MrStoreCMS/static/js/runtime-main.3e71f96d.js"
+  }
+]);
